df5cb0fd573592731ed8f7752da1cf6f65d540bb
Luis Fernando Posada Cano <lfposadac@eafit.edu.co>
2021-08-01 17:21:19 -0500

    Final, no me dio para la persistencia por fallas con MySQL sorry :(

--- ListaGanadores.java
+++ ListaGanadores.java
@@ -5,7 +5,49 @@
  * @author (Luis Fernando Posada Cano) 
  * @version (01/08/2021)
  */
+/*
+ * Serializable es una clase ubicada en el paquete Java. io. Serializable, la cual no cuenta con 
+ * ningún método, por lo que es una clase que sirve solamente para especificar que todo el estado
+ * de un objeto instanciado podrá ser escrito o enviado en la red como una trama de bytes
+ */
+import java.io.Serializable;
 public class ListaGanadores
 {
+    public int id;
+    public int cantidadPrimero;
+    public int cantidadSegundo;
+    public int cantidadTercero;
+    public String name;
     
-}
+    public ListaGanadores(){}
+    
+    public ListaGanadores(int id, String name, int cantidadPrimero, int cantidadSegundo, int cantidadTercero){
+      this.id = id;
+      this.name = name;
+      this.cantidadPrimero = cantidadPrimero;
+      this.cantidadSegundo = cantidadSegundo;
+      this.cantidadTercero = cantidadTercero;
+    }
+    
+    public int getId(){ return id; }
+    
+    public String getName() { return name; }
+    
+    public int getCantidadPrimero() { return cantidadPrimero; }
+    
+    public int getCantidadSegundo() { return cantidadSegundo; }
+    
+    public int getCantidadTercero() { return cantidadTercero; }
+    
+    public void setId(int id) { this.id = id; }
+    
+    public void setName(String name) { this.name = name; }
+    
+    public void setCantidadPrimero(int cantidadPrimero){ this.cantidadPrimero = cantidadPrimero;}
+    
+    public void setCantidadSegundo(int cantidadSegundo){ this.cantidadSegundo = cantidadSegundo;}
+    
+    public void setCantidadTercero(int cantidadTercero){ this.cantidadTercero = cantidadTercero;}
+    
+    }
+
